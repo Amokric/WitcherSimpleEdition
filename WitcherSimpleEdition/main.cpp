f38dacace9b6aa2d94022b8aee0bfef12d4b4b41
Aleksandra <aleks.mokric@gmail.com>
2018-11-16 00:41:54 +0100

    Initial Commit

---
+++ WitcherSimpleEdition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WitcherSimpleEdition
+//
+//  Created by Aleksandra Mokric on 16/11/2018.
+//  Copyright © 2018 Aleksandra Mokric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
